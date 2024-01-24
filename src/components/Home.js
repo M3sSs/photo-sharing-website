e585247cdd477629f9d657b6eed7e282bf3d3b9c
Arun Uniyal <ARUNUNIYAL.21011166@gehu.ac.in>
2024-01-24 22:40:26 +0530

    Photo.js created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './HomeStyle.css'
 
 
 
 export default function Home() {
-
+  useEffect(()=>{
+    console.log("aaa")
+  },[])
   return (
     <div className='container'>
       
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,5 +1,5 @@
-
 import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import './PhotosStyle.css';
 import { createApi } from 'unsplash-js';
 import PhotoCard from './PhotoCard';
@@ -10,26 +10,54 @@ const unsplash = createApi({
 });
 
 const Photos = () => {
-  const [url, setUrl] = useState('');
-  useEffect(() => {
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchPhotos = () => {
     // Fetch data from Unsplash API
     unsplash.photos.getRandom({
-      count: 6,
+      count: 30,
+      page: page,
     }).then(result => {
-      const imageUrl = result.response[0].urls.full;
-      console.log(result);
-      console.log(imageUrl);
+      console.log(result)
+      if (result.errors) {
+        // handle error here
+        console.log('error occurred: ', result.errors[0]);
+      } 
+      else{
+        const newPhotos = result.response.map(photo => ({
+          id: photo.id,
+          imageUrl: photo.urls.full,
+        }));
 
-      // Set the received URL in the state
-      setUrl(imageUrl);
+        // Update the state with new photos
+        setPhotos(prevPhotos => [...prevPhotos, ...newPhotos]);
+  
+        // Increment the page for the next fetch
+        setPage(page + 1);
+      }
+
     });
+  };
+
+  useEffect(() => {
+    fetchPhotos();
   }, []); // The empty dependency array ensures that the effect runs once when the component mounts
 
   return (
-    <header className='container'>
+    <div className='container'>
       <h1>Photo Sharing Website</h1>
-      <PhotoCard imageUrl={url}/>
-    </header>
+      <InfiniteScroll
+        dataLength={photos.length}
+        next={fetchPhotos}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        {photos.map(photo => (
+          <PhotoCard key={photo.id} imageUrl={photo.imageUrl} />
+        ))}
+      </InfiniteScroll>
+    </div>
   );
 };
 
