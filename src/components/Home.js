afdbc994c9f0726e0869b15f15c86f5349dae3e8
Arun Uniyal <ARUNUNIYAL.21011166@gehu.ac.in>
2024-02-05 21:45:45 +0530

    new home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
 import React, { useEffect } from 'react'
-import './HomeStyle.css'
+import './HomeStyle2.css'
 
 
 
 export default function Home() {
   
   return (
-    <div className='container'>
+    <div className='Container'>
       
       <div className="Heading">
         <h1>Explore Your Thing</h1>
       </div>
 
       <div className="main-categories">
-       
+        
+        <div id='child1'>
         <a href="/travel"  className='Travel'>
           <p className='Text'>Travel</p>
           </a>
@@ -25,9 +26,9 @@ export default function Home() {
           <p className='Text'>Landscapes</p>
           {/* <a href=""><button className='Explorer'>Explore</button></a> */}
         </a>
-      </div>
+        </div>
 
-      <div className="other-categories">
+        <div id='child2'>
         <a href='/animals' className="animals">
         <p className='Text'>animals</p>
           {/* <a href=""><button className='Explorer'>Explore</button></a> */}
@@ -36,6 +37,9 @@ export default function Home() {
         <p className='Text'>Cinema</p>
           {/* <a href=""><button className='Explorer'>Explore</button></a> */}
         </a>
+        </div>
+
+        <div id='child3'>
         <a href='/scifi' className="SciFi">
         <p className='Text'>Scifi</p>
           {/* <a href=""><button className='Explorer'>Explore</button></a> */}
@@ -44,24 +48,32 @@ export default function Home() {
         <p className='Text'>Inspiring</p>
           {/* <a href=""><button className='Explorer'>Explore</button></a> */}
         </a>
+        </div>
+        
+        <div id='child4'>
         <a href='/seasonal' className="Seasonal">
         <p className='Text'>Seasonal</p>
           {/* <a href=""><button className='Explorer'>Explore</button></a> */}
         </a>
+
         <a href='/artistic' className="artistic">
         <p className='Text'>Artisitc</p>
           {/* <a href=""><button className='Explorer'>Explore</button></a> */}
         </a>
-        <a href='/anime' className="anime">
-        <p className='Text'>Anime</p>
-          {/* <a href=""><button className='Explorer'>Explore</button></a> */}
-        </a>
-        <a href='/fantasy' className="fantasy">
-          <p className='Text'>Fantasay</p>
-          {/* <a href=""><button className='Explorer'>Explore</button></a> */}
-        </a>
-      </div>
-        
+        </div>
+
+        <div id='child5'>
+          <a href='/anime' className="anime">
+          <p className='Text'>Anime</p>
+            {/* <a href=""><button className='Explorer'>Explore</button></a> */}
+          </a>
+          <a href='/fantasy' className="fantasy">
+            <p className='Text'>Fantasay</p>
+            {/* <a href=""><button className='Explorer'>Explore</button></a> */}
+          </a>
+        </div>
+      
+        </div>
     </div>
   )
 }
